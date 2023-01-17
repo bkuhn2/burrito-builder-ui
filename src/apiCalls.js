2afdf42d21988b7655e0a6d61dd77e7c9fec4ab4
Brett Kuhn <brettkuhn87@gmail.com>
2023-01-17 10:33:54 -0700

    Wrote function to post order

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -2,3 +2,12 @@ export const getOrders = () => {
   return fetch('http://localhost:3001/api/v1/orders')
       .then(response => response.json())
 }
+
+export const sendOrder = (order) => {
+  return fetch('http://localhost:3001/api/v1/orders', {
+    method: 'POST',
+    headers: {'Content-type': 'application/json'},
+    body: JSON.stringify(order)
+  })
+    .then(response => response.json())
+}
